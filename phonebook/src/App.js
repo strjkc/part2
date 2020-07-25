551ec96a3594c42a35c847f93cdc166bb1067000
strahinja <strahinjaa.va@gmail.com>
2020-07-26 01:44:40 +0200

    filter functionality

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,45 +1,58 @@
 import React, { useState } from 'react'
 
 
-const Person = ({name}) => {
+const Person = ({contact}) => {
+  console.log(contact)
   return(
-    <div>{name}</div>
+  <div>{contact.name}: {contact.number}</div>
   )
 }
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchValue, setSearchValue] = useState('')
 
   const nameInput = event => setNewName(event.target.value)
-  
+  const numberInput = event => setNewNumber(event.target.value)
+  const searchInput = event => setSearchValue(event.target.value)
+
+  const peopleToDisplay = searchValue.length > 0
+  ? persons.filter(object => object.name.toLowerCase().includes(searchValue.toLowerCase()) )
+  : persons
   const submitForm = event => {
     event.preventDefault()
-    const listOfNames = persons.map(object => object.name)
-    if (listOfNames.includes(newName)){
-      alert(`${newName} already added to the phonebook`)  
-    } else {
-      const newPerson = {name: newName}
-      setPersons(persons.concat(newPerson))  
-    } 
+    persons.map(object => object.name).includes(newName)
+    ? alert(`${newName} already added to the phonebook`)  
+    : setPersons(persons.concat({name: newName, number: newNumber}))   
       }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={searchValue} onChange={searchInput} />
+      </div>
+      <h2>Add new</h2>
       <form onSubmit={submitForm}>
         <div>
           name: <input value={newName} onChange={nameInput} />
         </div>
-        <div>debug: {newName}</div>
+        <div>
+          nubmer: <input value={newNumber} onChange={numberInput} />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(object => <Person key={object.name} name={object.name} />)}
+        {peopleToDisplay.map(object => <Person key={object.name} contact={object} />)}
     </div>
   )
 }
