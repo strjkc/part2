e930191c396915eb6b2fb2cc9f5dfbcd15869182
strahinja <strahinjaa.va@gmail.com>
2020-07-26 22:52:43 +0200

    initialize data with effect hook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Persons from './Persons'
 import PersonForm from './PersonForm'
 import Filter from './Filter'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchValue, setSearchValue] = useState('')
+
+  useEffect( () => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then( response => { console.log(response); setPersons(response.data) })
+  },[])
+
 
   const peopleToDisplay = searchValue.length > 0
   ? persons.filter(object => object.name.toLowerCase().includes(searchValue.toLowerCase()) )
